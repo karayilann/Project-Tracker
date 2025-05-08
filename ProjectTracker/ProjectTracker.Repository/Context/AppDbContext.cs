a70bb6fdfccf1f43e22dcd14a34467a7c9fb9944
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-05-09 01:34:57 +0300

    Created DB

---
+++ ProjectTracker/ProjectTracker.Repository/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectTracker.Core.Entities;
+using Task = ProjectTracker.Core.Entities.Task;
+
+namespace ProjectTracker.Repository.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MAMIROG\\MAMIROG;Initial Catalog=ProjectTrackerDB;Integrated Security=True;TrustServerCertificate=True;");
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }  
+        
+    }
+}

2ab9a3598e315801cdabf976986a8039ff25848f
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-05-11 03:10:31 +0300

    Created IGenericRepository & IUnitOfWork

---
+++ ProjectTracker/ProjectTracker.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectTracker.Core.Entities;
+using Task = ProjectTracker.Core.Entities.Task;
+
+namespace ProjectTracker.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Project> Projects { get; set; }
+        IGenericRepository<User> Users { get; set; }
+        IGenericRepository<Task> Tasks { get; set; }
+        IGenericRepository<Role> Roles { get; set; }
+
+    }
+}
